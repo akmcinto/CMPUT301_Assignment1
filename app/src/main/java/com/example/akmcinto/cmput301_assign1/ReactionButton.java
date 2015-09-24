138772817929d850137085e25208d1f14a4d707b
akmcinto <akmcinto@ualberta.ca>
2015-09-24 15:28:35 -0600

    Created new project using lab machines to change build lab-compatible version. Copied files from original project in.  May need further additions/debugging.

---
+++ app/src/main/java/com/example/akmcinto/cmput301_assign1/ReactionButton.java
@@ -0,0 +1,34 @@
+package com.example.akmcinto.cmput301_assign1;
+
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Andrea McIntosh on 17/09/2015.
+ */
+public class ReactionButton {
+
+    Button button;
+
+    public ReactionButton(Button button) {
+        this.button = button;
+    }
+
+    public Long GetDelay() {
+        Double delay = Math.random();
+        // Convert delay to be up to 2000ms
+        delay = delay * 2000;
+        // Make sure delay is at least 10ms
+        if (delay < 10) {
+            delay = Math.random();
+            delay = delay * 2000;
+        }
+        // For .wait() function, must return long
+        return delay.longValue();
+    }
+
+}
---
+++ app/src/main/java/com/example/akmcinto/cmput301_assign1/ReactionTimerActivity.java
@@ -0,0 +1,63 @@
+package com.example.akmcinto.cmput301_assign1;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class ReactionTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reaction_timer);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_reaction_timer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void ControlButton(View view) {
+        Button button = (Button) findViewById(R.id.reactionButton);
+        ReactionButton reactionButton = new ReactionButton(button);
+        // Start button's hide/show behaviour
+        Long delay = reactionButton.GetDelay();
+
+        button.setVisibility(View.INVISIBLE);
+
+        Handler timerHandler = new Handler();
+        timerHandler.removeCallbacks(waitRunnable);
+        timerHandler.postDelayed(waitRunnable, delay);
+    }
+
+    Runnable waitRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Button button = (Button) findViewById(R.id.reactionButton);
+            button.setVisibility(View.VISIBLE);
+        }
+    };
+
+}
